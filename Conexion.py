e40bfabea1412d8edeb92f65d22f798301818d6f
guido-perez <92828716+guido-perez@users.noreply.github.com>
2021-12-01 22:04:27 -0300

    Update Conexion
    Conexion por Instancia

--- Conexion.py
+++ Conexion.py
@@ -4,5 +4,9 @@ class Conexion:
     
     
     lib_dir = "C:\instantclient_21_3"
-    cx_Oracle.init_oracle_client(lib_dir=lib_dir)
-    conexion= cx_Oracle.connect(user="ADMIN", password="Inacap2021.-", dsn="dbprueba_high")
+    try:
+        cx_Oracle.init_oracle_client(lib_dir=lib_dir)
+        conexion= cx_Oracle.connect(user="ADMIN", password="Inacap2021.-", dsn="dbprueba_high")
+        
+    except Exception as ex:
+        print(ex)
