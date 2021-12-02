e7cd5bc5853113a07d522eea0126d925ed64ff5b
guido-perez <92828716+guido-perez@users.noreply.github.com>
2021-12-02 00:51:42 -0300

    Update Main

--- Main.py
+++ Main.py
@@ -1,49 +1,68 @@
 import sys
 
-import Estudiante
-import Docente
-import JefeDeCarrera
+from Matricula import Matricula
+
+#import Estudiante
+#import Docente
+#import JefeDeCarrera
 
 #Creacion de Menu con las opciones basicas de seleccion
-def Menu():
-    print("***************************************")
-    print("---- Bienvenidos al Sistema Inacap  ---\n")
-    print("1) Estudiantes") # Invoca a la clase Estudiantes
-    print("2) Docentes") # Invoca a la Clase Docentes
-    print("3) Jefe de Carrera") # Invoca a la Clase JefedeCarrera
-    print("\n4) Salir") # Salimos del programa
-    print("\n***************************************")
+def MenuPrincipal():
     
-    
+    print("==============================================================")
+    print("-------------- Bienvenidos al Sistema Inacap  ---------------")
+    print("====================== MENÚ PRINCIPAL ======================   ")
+    print("1.- Estudiantes                                               ") # Invoca a la clase Estudiantes
+    print("2.- Docentes                                                  ") # Invoca a la Clase Docentes
+    print("3.- Jefe de Carrera                                           ") # Invoca a la Clase JefedeCarrera
+    print("\n4.- Salir                                                   ") # Salimos del programa
+    print("\n=============================================================")
+
+
+
 op=0
 while not op == 4:
     
-    Menu()
-    #Convertimos la variable "op" a tipo Int
-    op=int(input()) 
+    MenuPrincipal()
+    op=int(input("Seleccione una opcion: "))
     
     if op > 0 and op <= 4:
         #se cumplan ambas opciones
         if op == 1:
-            print("Ingresando seccion Estudiante")
-            # Invocamos a clase personas para validar usuario y contraseña
-            # Incocamos Clase Estudiantes
+            print("\nIngresando seccion Estudiante\n")
+            print("¿El Alumno Matriculado? (S/N)\n")
+            sino = input("Ingrese una opcion: ")
+            if sino == "S" or "s":
+                print("\nAccediento a Portal Estudiante\n")
+ 
+            # Invocamos a clase personas para val1idar usuario y contraseña
+            # Invocamos Clase Estudiantes
+
+            if sino == "N" or "n":
+
+                print("\nAccediento al Formulario Matricula\n")
+                Mat = Matricula()
+                Mat.ingresarMatricula()
+            # Invocamos a Clase Matricula para ingresar un Alumno
+
+                
 
         if op == 2:
             print("Ingresando seccion Docentes")
+            sys.exit()
             # Invocamos a clase personas para validar usuario y contraseña
             # Incocamos Clase Docentes
             
         if op == 3:
             print("Ingresando seccion Jefe de Carrera")
+            sys.exit()
             # Invocamos a clase personas para validar usuario y contraseña
             # Incocamos Clase Jefe de Carrera
 
         if op == 4:
             sys.exit("Saliendo del sistema")
-
-
         
     else:
-        print("Opcion seleccionada no valida")
+        print("\n--- Opcion seleccionada no valida ---")
+        print("--- Vuelva a seleccionar una de las alternativas del Menu --- \n")
         
