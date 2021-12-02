4b05c3a1efc45f2bf96f9fda20f350bd9eb7ebf3
guido-perez <92828716+guido-perez@users.noreply.github.com>
2021-12-01 22:02:41 -0300

    Add files via upload

--- Conexion.py
+++ Conexion.py
@@ -1,18 +1,8 @@
 import cx_Oracle
-lib_dir = "C:\instantclient_21_3"
-try:
+
+class Conexion:
+    
+    
+    lib_dir = "C:\instantclient_21_3"
     cx_Oracle.init_oracle_client(lib_dir=lib_dir)
     conexion= cx_Oracle.connect(user="ADMIN", password="Inacap2021.-", dsn="dbprueba_high")
-    
-    cursor = conexion.cursor()
-   # data=cursor.execute("select * from docente")
-   # data.fetchall()
-   # print(cursor)
-    for row in cursor.execute("select * from docente"):
-        print(row)
-        
-except Exception as ex:
-    print(ex)
-
-
-
